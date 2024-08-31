fcdc4e00729ed481a06117adad90848aa1c6dfff
mohammed al-kilani <m.sh64944@gmail.com>
2024-08-31 16:53:06 +0300

    ./

--- run.js
+++ run.js
@@ -1,7 +1,8 @@
 // const { exec  } = require('child_process');
 // exec('get.exe');
+const path = require('path');
 const spawn = require('child_process').spawn
-const ls    = spawn('./get');
+const ls    = spawn(path.join(__dirname, 'get.exe'));
 
 ls.stdout.on('data', function (data) {
   console.log('stdout: ' + data.toString());
